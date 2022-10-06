1cd75ae112c895ec0787730fca8f42399305b6a9
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2022-10-06 21:04:02 +0300

    Update Router.tsx

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,14 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 
 import HomePage from "pages/home";
 import NotFound from "pages/error";
 
 const Router = () => {
 	return (
-		<Routes>
-			<Route path="/" element={<HomePage />} />
-			<Route path="*" element={<NotFound />} />
-		</Routes>
+		<BrowserRouter basename={!process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? "" : "/anime-roulette"}>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
