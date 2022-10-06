c7c9806573501cccb410aa8ba47d9dbccf68b36d
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2022-10-06 21:13:02 +0300

    Update PackagesProviders.tsx

--- src/config/PackagesProviders/PackagesProviders.tsx
+++ src/config/PackagesProviders/PackagesProviders.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const PackagesProviders: React.FC<Props> = ({children}) => {
   return (
-    <BrowserRouter basename={!process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? "" : "/anime-roulette"}>
+    <BrowserRouter basename={"/anime-roulette"}>
       <ChakraProvider theme={theme} cssVarsRoot="html">
         {children}
       </ChakraProvider>
