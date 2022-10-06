116f7b882db2d1d36bbf2a8b4989b6e38453fb84
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2022-10-06 21:10:10 +0300

    Fix router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,12 +5,10 @@ import NotFound from "pages/error";
 
 const Router = () => {
 	return (
-		<BrowserRouter basename={!process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? "" : "/anime-roulette"}>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
-		</BrowserRouter>
 	);
 }
 
